851ed2eed6c8816347366cf1b2d74734e54075bf
Waeez <waeez1999@gmail.com>
2022-05-13 23:15:56 +0500

    phase1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,21 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <div class="card bg-primary" style="width: 18rem;">
+            <div class="card bg-primary">
                 <div class="card-body text-white">
                   <h5 class="card-title">Application Form</h5>
                   <p class="card-text"></p>
                   <a href="{{ route('addApplicant') }}" class="btn btn-light">Add New Applicant</a>
+                </div>
+              </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card bg-success">
+                <div class="card-body text-white">
+                  <h5 class="card-title">All Applications</h5>
+                  <p class="card-text"></p>
+                  <a href="{{ route('addApplicant') }}" class="btn btn-light">View All Applications</a>
                 </div>
               </div>
         </div>
